fb00a2a8c788bc6654d974d4d4678ee390b4bdee
Christian De Kievit <cdekievit@gmail.com>
2014-11-05 20:58:10 +1100

    Added repositories and also bootstrap

--- ss-dashboard/app/router.js
+++ ss-dashboard/app/router.js
@@ -6,6 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('repository', { path: 'repositories/:repository_id' }, function() { });
 });
 
 export default Router;
